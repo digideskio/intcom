2e8babb34c9c745b854e470af672a49ab05c741f
Jelle Luteijn <jelle.luteijn@gmail.com>
2015-03-16 21:35:32 +0100

    very secret external auth check

--- src/main/java/nl/hu/v2iac1/secretpages/VerysecretServlet.java
+++ src/main/java/nl/hu/v2iac1/secretpages/VerysecretServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import nl.hu.v2iac1.Configuration;
 
 /**
@@ -31,19 +32,30 @@ public class VerysecretServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Configuration configuration = new Configuration();
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Verysecret</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Verysecret</h1>");
-            out.println("This is very secret: " + configuration.getValue(Configuration.Key.VERYSECRET));
-            out.println("</body>");
-            out.println("</html>");
+        HttpSession session = request.getSession();
+        if(session.getAttribute("username") != null && session.getAttribute("externaltoken") != null) {
+            String username = session.getAttribute("username").toString();
+            String logintoken = session.getAttribute("externaltoken").toString();
+            if(logintoken.equals("1") && logintoken != null) {
+                response.setContentType("text/html;charset=UTF-8");
+                try (PrintWriter out = response.getWriter()) {
+                    /* TODO output your page here. You may use following sample code. */
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>Verysecret</title>");            
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1>Verysecret</h1>");
+                    out.println("This is very secret: " + configuration.getValue(Configuration.Key.VERYSECRET));
+                    out.println("</body>");
+                    out.println("</html>");
+                }
+            }else{
+                response.sendRedirect("/sample/external");
+            }
+        }else{
+            response.sendRedirect("/sample/external");
         }
     }
 
