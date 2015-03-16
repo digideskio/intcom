755e203a2fd2f76517f2204a21fd5a1f438ff312
Jelle Luteijn <jelle.luteijn@gmail.com>
2015-03-16 14:10:07 +0100

    initial commit

---
+++ src/main/java/nl/hu/v2iac1/rest/resource/SecretRestService.java
@@ -0,0 +1,26 @@
+package nl.hu.v2iac1.rest.resource;
+
+import nl.hu.v2iac1.Configuration;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/secret")
+@Produces(MediaType.TEXT_PLAIN)
+public class SecretRestService extends AbstractRestService{
+
+
+
+    @GET
+    @Path("/")
+    public Response getSecret() {
+
+        String output = "This is the secret: " + configuration.getValue(Configuration.Key.SECRET);
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
